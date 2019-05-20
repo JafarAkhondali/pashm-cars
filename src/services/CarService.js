519c84a1d3a6d29db29efd11e88e539850ae99a5
Alireza Km <alitm28@gmail.com>
2019-05-20 21:28:07 +0430

    http urls compatible with backend.

--- src/services/CarService.js
+++ src/services/CarService.js
@@ -6,12 +6,12 @@ const command  = (statusBool) => {
 
 export function engineStart(status) {
     let data = command(status);
-    return Http().post('/engine', data);
+    return Http().post('/engine/', data);
 }
 
 export function horn(status) {
     let data = command(status);
-    return Http().post('/beep', data);
+    return Http().post('/beep/', data);
 }
 
 export function BlobToBase64(blob){
@@ -30,32 +30,32 @@ export function BlobToBase64(blob){
 
 export function water(status) {
     let data = command(status);
-    return Http().post('/water', data);
+    return Http().post('/water/', data);
 }
 
 
 export function assistant(file) {
-    return Http().post('/assistant', {file});
+    return Http().post('/assistant/', {file});
 }
 
 export function lockDoors(status) {
     let data = command(status);
-    return Http().post('/assistant', data);
+    return Http().post('/lock/', data);
 }
 
 
 export function display(data) {
-    return Http().post('/display', {data});
+    return Http().post('/display/', {data});
 }
 
 
 
 /**
  *
- * @param position - Can be only lf or rf ( LeftFront - RightFront)
+ * @param position - Can be only front_left or front_right
  * @param state - Can be only up, down or stop
  * @returns Promise
  */
 export function windowControll(position, state) {
-    return Http().post(`/window/${position}`, {state});
+    return Http().post(`/window/${position}/`, {state});
 }
